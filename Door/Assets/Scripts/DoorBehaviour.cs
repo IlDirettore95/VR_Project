716aa9374f533a88ce7bebd7f844a778782680f3
simon420 <metalrulez@live.it>
2021-03-31 14:23:36 +0200

    updated door

---
+++ Door/Assets/Scripts/DoorBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorBehaviour : MonoBehaviour
+{
+    public GameObject leftDoor;
+
+    public GameObject rightDoor;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject obj = other.GetComponent<GameObject>();
+        if (obj != null)
+            openDoor();
+        
+        
+    }
+
+    private void openDoor()
+    {
+        leftDoor.transform.Translate(-1.25F,0,0);
+        rightDoor.transform.Translate(1.25F,0,0);
+    }
+}
