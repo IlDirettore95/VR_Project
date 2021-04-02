21711e4f5a768418049b229a8643325b23ae3ee8
Dannghal <75496817+Dannghal@users.noreply.github.com>
2021-04-02 19:23:32 +0200

    Added AnimatedArmV2

---
+++ Arm Project/Assets/Scripts/Animation/ArmStateController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmStateController : MonoBehaviour
+{
+    Animator animator;
+    Shooter shooter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        shooter = GetComponentInParent<Shooter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isGrabbing = shooter.GetIsAttracting();
+        bool isThrowing = shooter.GetIsLaunching();
+        bool isReleasing = shooter.GetIsReleasing();
+        bool isRepulsing = shooter.GetIsRepulsing();
+
+        if (isGrabbing)
+        {
+            animator.SetBool("IsGrabbing", true);
+        }
+        else animator.SetBool("IsGrabbing", false);
+
+        if (isThrowing)
+        {
+            animator.SetBool("IsThrowing", true);
+        }
+        else animator.SetBool("IsThrowing", false);
+
+        if (isReleasing)
+        {
+            animator.SetBool("IsReleasing", true);
+        }
+        else animator.SetBool("IsReleasing", false);
+
+        if (isRepulsing)
+        {
+            animator.SetBool("IsRepulsing", true);
+        }
+        else animator.SetBool("IsRepulsing", false);
+    }
+}
