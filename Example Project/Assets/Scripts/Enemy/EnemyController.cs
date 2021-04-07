8e0c1d04a260bf122e93490e4aa72a175c2575f0
Dannghal <75496817+Dannghal@users.noreply.github.com>
2021-04-08 01:21:11 +0200

    Spider material and light

---
+++ Example Project/Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Rigidbody))]
+public class SimpleIA1 : MonoBehaviour
+{
+    public float movementSpeed = 4f;
+    public float attackDistance = 3f;
+    public float npcHP = 100;
+    public float npcDamage = 5;
+    public float attackRate = 0.5f;
+    float nextAttackTime = 0;
+    public Transform firePoint;
+
+    [HideInInspector]
+    public Transform playerTransform;
+    private GameObject player;
+
+    NavMeshAgent agent;
+    Rigidbody r;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = movementSpeed;
+        r = GetComponent<Rigidbody>();
+        r.useGravity = false;
+        r.isKinematic = true;
+        player = GameObject.Find("Player");
+        playerTransform = player.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (agent.remainingDistance - attackDistance < 0.01f)
+        {
+            if (Time.time > nextAttackTime)
+            {
+                nextAttackTime = Time.time + attackRate;
+
+                //Attack
+                RaycastHit hit;
+                if (Physics.Raycast(firePoint.position, firePoint.forward, out hit, attackDistance))
+                {
+                    if (hit.transform.CompareTag("Player"))
+                    {
+                        Debug.DrawLine(firePoint.position, firePoint.position + firePoint.forward * attackDistance, Color.cyan);
+                    }
+                }
+            }
+        }
+
+        //Move towardst he player
+        agent.destination = playerTransform.position;
+        //Always look at player
+        transform.LookAt(new Vector3(playerTransform.transform.position.x, transform.position.y, playerTransform.position.z));
+        //Gradually reduce rigidbody velocity if the force was applied by the bullet
+        r.velocity *= 0.99f;
+    }
+
+    public void Hurt(float damage)
+    {
+        npcHP -= damage;
+        Debug.Log("Enemy hurted! HP: " + npcHP);
+        if (npcHP <= 0)
+            Destroy(gameObject);
+    }
+}
---
+++ Example Project/Assets/Scripts/Enemy/ReactionIA.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PerceptionIA))]
+[RequireComponent(typeof(Rigidbody))]
+public class ReactionIA : MonoBehaviour
+{
+    PerceptionIA _perception;
+    Rigidbody _rb;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _perception = GetComponent<PerceptionIA>();
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!_perception.IsAllerted())
+        {
+            //Just stand
+        }
+        else
+        {
+
+            transform.LookAt(_perception.GetPlayerPosition());
+            _rb.MovePosition(transform.position + transform.forward * speed * Time.deltaTime);
+        }
+    }
+}
