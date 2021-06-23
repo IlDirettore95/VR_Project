76721e836124cb287a5a86ee729119a2ecf86115
Dannghal <75496817+Dannghal@users.noreply.github.com>
2021-06-23 19:55:16 +0200

    Working on SaveSystem

---
+++ SaveSystem/Assets/Scripts/SaveSystem/SaveSystem.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class SaveSystem
+{
+    public static bool Save(object saveData, string saveName) //Input: (Player player)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+
+        if(!Directory.Exists(Application.persistentDataPath + "/saves"))
+        {
+            Directory.CreateDirectory(Application.persistentDataPath + "/saves");
+        }
+
+        string path = Application.persistentDataPath + "/saves/" + saveName + ".save";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        formatter.Serialize(stream, saveData);
+        stream.Close();
+
+        return true;
+    }
+
+    public static object Load(string saveName)
+    {
+        string path = Application.persistentDataPath + "/saves/" + saveName + ".save";
+
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            object data = formatter.Deserialize(stream);
+            stream.Close();
+
+            return data;
+        }
+        else
+        {
+            Debug.LogError("Save file not found at " + path);
+            return null;
+        }
+    }
+}
