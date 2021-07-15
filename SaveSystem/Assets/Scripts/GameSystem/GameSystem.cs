492ff719938bb89ed7ab6749db0d9395218f5250
Dannghal <75496817+Dannghal@users.noreply.github.com>
2021-07-15 17:48:38 +0200

    Level 1
    - Aggiunte ringhiere
    - Aggiunte porte
    - Aggiunti oggetti nella sala del jetpack

---
+++ SaveSystem/Assets/Scripts/GameSystem/GameSystem.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSystem : MonoBehaviour
+{
+    public enum Scene
+    {
+        Level1,
+        Level2,
+        Loading
+    }
+
+    private Scene _currentLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NextLevel()
+    {
+        switch (_currentLevel)
+        {
+            case Scene.Level1:
+                {
+                    _currentLevel = Scene.Level2;
+                    SaveSystem.Save(_currentLevel.ToString(), "save");
+                    Loader.Load(_currentLevel.ToString());
+
+                    break;
+                }
+        }
+    }
+
+    public void NewGame()
+    {
+        _currentLevel = Scene.Level1;
+        SaveSystem.Save(_currentLevel.ToString(), "save");
+        Loader.Load(_currentLevel.ToString());
+    }
+
+    public void Continue()
+    {
+        _currentLevel = (Scene)System.Enum.Parse(typeof(Scene),SaveSystem.Load("save"));
+        Loader.Load(_currentLevel.ToString());
+    }
+}
---
+++ SaveSystem/Assets/Scripts/GameSystem/SceneManager/NextLevelTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextLevelTrigger : MonoBehaviour
+{
+    public GameSystem gameSys;
+    public string nextLevel;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        gameSys.NextLevel();
+    }
+}
