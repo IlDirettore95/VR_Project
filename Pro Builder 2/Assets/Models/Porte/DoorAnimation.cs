3258f07f7e4902ad91178e59c459fc73a36cd3b9
IlDirettore95 <gmdg141995@gmail.com>
2021-07-22 20:14:29 +0200

    Final build updated

--- Pro Builder 2/Assets/Models/Porte/DoorAnimation.cs
+++ Pro Builder 2/Assets/Models/Porte/DoorAnimation.cs
@@ -24,6 +24,7 @@ public class DoorAnimation : MonoBehaviour
         GameObject pl = other.gameObject;
         if (pl != null && pl.tag.Equals("Player"))
         {
+            Debug.Log("entra");
             _animator.SetBool("isOpening", true);
         }
 
