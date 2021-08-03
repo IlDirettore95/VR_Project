9b8297f8514c3b72c5fd462324228d042a6c8943
simon420 <crvsmn97h27h579l@studenti.unical.it>
2021-08-03 18:50:47 +0200

    added a new intro scene before main menu scene
    the new scene contains an intro video showing team logo and other hilarious things.

---
+++ Unified2/Assets/Scripts/HUD/VideoToMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class VideoToMenu : MonoBehaviour
+{
+
+    [SerializeField] private VideoPlayer _videoPlayer;
+
+   
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(checkVideoPlayer());
+       
+        
+    }
+
+    public IEnumerator checkVideoPlayer()
+    {
+        
+        
+        yield return new WaitForSeconds((float)_videoPlayer.length);
+        Debug.Log("finito");
+
+        SceneManager.LoadScene("MainMenu");
+
+    }
+}
